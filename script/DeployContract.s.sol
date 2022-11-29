5db9378a424cea9d0b5889374ae50e9801b4a4ad
efecarranza <fcarr004@fiu.edu>
2022-11-29 12:32:58 -0500

    add contracts for rate strategy

--- script/DeployContract.s.sol
+++ script/DeployContract.s.sol
@@ -1,16 +1,27 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.15;
+pragma solidity 0.6.12;
 
 import "@forge-std/console.sol";
 import {Script} from "@forge-std/Script.sol";
-import {Contract} from "../src/Contract.sol";
+import {DefaultReserveInterestRateStrategye} from "../src/DefaultReserveInterestRateStrategy.sol";
+import {ILendingPoolAddressesProvider} from "../src/interfaces/ILendingPoolAddressesProvider.sol";
+import {AaveV2Ethereum} from "@aave-address-book/AaveV2Ethereum.sol";
 
 contract DeployContract is Script {
-    uint256 public constant NUM = 7;
-
     function run() external {
         vm.startBroadcast();
-        Contract deployedContract = new Contract(NUM);
+        ILendingPoolAddressesProvider provider = ILendingPoolAddressesProvider(
+            0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+        );
+        DefaultReserveInterestRateStrategy deployedContract = new DefaultReserveInterestRateStrategy(
+            provider,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        );
         console.log("Contract address: ", address(deployedContract));
         vm.stopBroadcast();
     }
