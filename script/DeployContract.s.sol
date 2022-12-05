d89a2317f9018675f9dc98825173c8950d0fa1ea
efecarranza <fcarr004@fiu.edu>
2022-12-05 11:03:25 -0500

    add diff tool and upate script

--- script/DeployContract.s.sol
+++ script/DeployContract.s.sol
@@ -3,9 +3,8 @@ pragma solidity 0.6.12;
 
 import "@forge-std/console.sol";
 import {Script} from "@forge-std/Script.sol";
-import {DefaultReserveInterestRateStrategy} from "../src/DefaultReserveInterestRateStrategy.sol";
-import {AaveV2Ethereum} from "@aave-address-book/AaveV2Ethereum.sol";
-import {ILendingPoolAddressesProvider} from "@aave-address-book/AaveV2.sol";
+import {DefaultReserveInterestRateStrategy} from "@aave-v2/protocol/lendingpool/DefaultReserveInterestRateStrategy.sol";
+import {ILendingPoolAddressesProvider} from "@aave-v2/interfaces/ILendingPoolAddressesProvider.sol";
 
 /*
 * Script to deploy new Interest Rate Strategy used by Aave V2 pools
@@ -25,10 +24,15 @@ import {ILendingPoolAddressesProvider} from "@aave-address-book/AaveV2.sol";
 */
 
 contract DeployContract is Script {
+    ILendingPoolAddressesProvider internal constant POOL_ADDRESSES_PROVIDER =
+        ILendingPoolAddressesProvider(
+            0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+        );
+
     function run() external {
         vm.startBroadcast();
         DefaultReserveInterestRateStrategy deployedContract = new DefaultReserveInterestRateStrategy(
-            AaveV2Ethereum.POOL_ADDRESSES_PROVIDER,
+            POOL_ADDRESSES_PROVIDER,
             800000000000000000000000000,
             0,
             57500000000000000000000000,
